47934b8eb6e4b7e657aad9cadd7fcb37d0be36e4
RITIK JAIN <72255272+mrritikjain@users.noreply.github.com>
2026-01-22 08:27:28 +0530

    Api feching app is created in react

--- Day5/ApiFetchApp/src/App.jsx
+++ Day5/ApiFetchApp/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [user, setUser] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [err, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      setError("");
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        if (!res.ok) throw new Error("Failed");
+        const result = await res.json();
+        setUser(result);
+        console.log(result);
+      } catch {
+        setError("Something went wrong");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading..</p>;
+  if (err) return <p>{err}</p>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h2>Users List</h2>
+      <ul>
+        {user.map((e) => (
+          <li key={e.id}>{e.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default App
+export default App;
